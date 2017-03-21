f90b5881d5bce7fb709e6ef8d9d28b9adb9b121f
Anna Horodetska and Starlynne Start <starlynne@>
2017-03-21 10:57:09 -0700

    Add hide/show function with jQuery, add images and content

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  $(".click-sweet").click(function() {
+    $(".donut").slideDown();
+
+  });
+  $(".donut button").click(function() {
+    $(".donut").slideUp();
+
+  });
+});
+
+$(document).ready(function() {
+  $(".click-juicy").click(function() {
+    $(".juicy").slideDown();
+
+  });
+  $(".juicy button").click(function() {
+    $(".juicy").slideUp();
+
+  });
+});
+
+$(document).ready(function() {
+  $(".click-drink").click(function() {
+    $(".drink").slideDown();
+
+  });
+  $(".drink button").click(function() {
+    $(".drink").slideUp();
+
+  });
+});
