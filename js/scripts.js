309e32996b1bd3f014680c1190ac8a49f8f6a30d
Anna Horodetska and Starlynne Start <starlynne@>
2017-03-21 11:29:49 -0700

    add jQuery to change class/styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
   $(".click-sweet").click(function() {
     $(".donut").slideDown();
+    $("body").removeClass();
+    $("body").addClass("pink");
 
   });
   $(".donut button").click(function() {
     $(".donut").slideUp();
+    // $("body").removeClass("pink");
 
   });
 });
@@ -12,10 +15,13 @@ $(document).ready(function() {
 $(document).ready(function() {
   $(".click-juicy").click(function() {
     $(".juicy").slideDown();
+    $("body").removeClass();
+    $("body").addClass("green")
 
   });
   $(".juicy button").click(function() {
     $(".juicy").slideUp();
+    // $("body").removeClass("green")
 
   });
 });
@@ -23,10 +29,13 @@ $(document).ready(function() {
 $(document).ready(function() {
   $(".click-drink").click(function() {
     $(".drink").slideDown();
+    $("body").removeClass();
+    $("body").addClass("blue")
 
   });
   $(".drink button").click(function() {
     $(".drink").slideUp();
 
   });
+
 });
